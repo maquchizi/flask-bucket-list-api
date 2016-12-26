73df99a547dacc90fc1e8ef86de5a2b7ea708a1e
Mark Ng'ang'a <maqnganga@gmail.com>
2016-12-26 17:53:24 +0300

    Write preliminary bucketlist tests

---
+++ tests/test_bucketlist.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+
+
+class TestBucketlist(TestCase):
+
+    def test_it_creates_bucketlist(self):
+        pass
+
+    def test_it_edits_bucketlist(self):
+        pass
+
+    def test_it_deletes_bucketlist(self):
+        pass
+
+    def test_it_fails_to_create_bucketlist_if_fields_missing(self):
+        pass
+
+    def test_does_not_list_bucketlists_if_not_logged_in(self):
+        pass
+
+    def test_it_does_not_create_buckelist_if_not_logged_in(self):
+        pass
+
+    def test_it_does_edit_bucketlist_if_not_logged_in(self):
+        pass
+
+    def test_it_does_not_delete_bucketlist_if_not_logged_in(self):
+        pass
+
+    def test_it_does_not_edit_list_not_owned_by_user(self):
+        pass
+
+    def test_it_does_not_delete_list_not_owned_by_user(self):
+        pass
+
+    def test_it_does_not_list_bucketlists_not_owned_by_user(self):
+        pass
