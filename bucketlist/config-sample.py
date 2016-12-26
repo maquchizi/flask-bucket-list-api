35442afa48709d059e3d63911f504424338dab82
Mark Ng'ang'a <maqnganga@gmail.com>
2016-12-26 20:50:17 +0300

    Finish modelling database
     * Postgres connection now works

--- bucketlist/config-sample.py
+++ bucketlist/config-sample.py
@@ -1,6 +1,7 @@
 from os import path
 
 ROOT = path.dirname(path.realpath(__file__))
-SQLALCHEMY_DATABASE_URI = 'postgres://username:password@server/flask-bucketlist'
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/flask-bucketlist'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'supersecretkey'
 DEBUG = True
--- bucketlist/models.py
+++ bucketlist/models.py
@@ -7,7 +7,7 @@ app.config.from_object(config)
 db = SQLAlchemy(app)
 
 
-class User(db.model):
+class User(db.Model):
     """
     User
     """
@@ -27,23 +27,29 @@ class User(db.model):
         return '<User %r>' % self.forename
 
 
-class Bucketlist(db.model):
+class Bucketlist(db.Model):
     """
     Bucketlist
     """
     list_id = db.Column(db.Integer, primary_key=True)
-    list_name = db.Column(db.String(255))
+    list_title = db.Column(db.String(255))
     list_description = db.Column(db.String(255))
     user = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    items = db.relationship('BucketlistItem', backref='bucketlist',
+                            lazy='dynamic')
     created = db.Column(db.DateTime())
     modified = db.Column(db.DateTime())
 
 
-class BucketlistItem(object):
+class BucketlistItem(db.Model):
     """
     BucketlistItem
     """
     item_id = db.Column(db.Integer, primary_key=True)
+    item_content = db.Column(db.String(255))
     bucketlist = db.Column(db.Integer, db.ForeignKey('bucketlist.list_id'))
     created = db.Column(db.DateTime())
     modified = db.Column(db.DateTime())
+
+
+db.create_all()
