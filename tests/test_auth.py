d6e2352d0f809c43a2ff79ba76c026171ae87ad9
Mark Ng'ang'a <maqnganga@gmail.com>
2017-01-05 11:06:26 +0300

    Make test_auth PEP8 again

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,22 +7,35 @@ class TestAuth(TestCase):
 
     def setUp(self):
         self.client = bucketlist.app.test_client()
+        self.correct_credentials = json.dumps({"email":
+                                              "mark.nganga@andela.com",
+                                               "password": "p@ssw0rd"})
+        self.incorrect_credentials = json.dumps({"email":
+                                                "mark.nganga@andela.com",
+                                                 "password": "wr0ngp@ssw0rd"})
 
     def test_it_allows_login(self):
-        response = self.client.post('/auth/login', data=json.dumps({"email": "mark.nganga@andela.com", "password": "p@ssw0rd"}), content_type='application/json')
+        response = self.client.post('/auth/login',
+                                    data=self.correct_credentials,
+                                    content_type='application/json')
 
         content = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
         self.assertIn('access_token', content)
 
     def test_login_fails_with_incorrect_credentials(self):
-        pass
+        response = self.client.post('/auth/login',
+                                    data=self.incorrect_credentials,
+                                    content_type='application/json')
 
-    def test_it_returns_token_on_login(self):
-        pass
+        self.assertEqual(response.status_code, 401)
 
     def test_login_method_only_allows_post_requests(self):
-        pass
+        response = self.client.get('/auth/login',
+                                   data=self.correct_credentials,
+                                   content_type='application/json')
+
+        self.assertEqual(response.status_code, 405)
 
     def test_login_error_does_not_specify_incorrect_field(self):
         pass
